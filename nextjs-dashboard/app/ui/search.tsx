25e9d144c96acfeb907c1012d08f0e82c2f7c104
danilogmarques <danilogustavomarques@gmail.com>
2025-07-04 09:31:38 -0300

    foi finalizado e capitulo 11, iniciando capitulo 12

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
@@ -14,13 +13,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
    const handleSearch = useDebouncedCallback((term) => {
 
     const params = new URLSearchParams(searchParams);
+     params.set('page', '1');
      if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-      replace(`${pathname}?${params.toString()}`);
-  }, 300);
+    replace(`${pathname}?${params.toString()}`);
+  })
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
